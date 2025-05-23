9f1edc86b18971a65ee6efa9f1f13389f983f1d3
poojitha <your.email@example.c>
2025-05-23 19:24:58 +0530

    finished working on bservices

--- BackgroundServices/index.js
+++ BackgroundServices/index.js
@@ -4,6 +4,8 @@ import dbConnection from "./utils/db.js";
 import cron from "node-cron";
 import sendWelcomeEmail from "./EmailServices/sendWelcomeEmail.js";
 import sendPendingOrderEmail from "./EmailServices/sendPendingOrderEmail.js"
+import sendDeliveredOrderEmail from "./EmailServices/sendDeliveredOrderEmail.js";
+import sendPromotionEmail from "./EmailServices/sendPromotionEmail.js";
 
 dotenv.config();
 const app = express();
@@ -14,14 +16,22 @@ const services = () => {
   cron.schedule("* * * * * *", () => {
     sendWelcomeEmail();
     sendPendingOrderEmail();
-
+    sendDeliveredOrderEmail();
     
+  });
+};
+
+const promotion = () => {
+  cron.schedule("30 5 * * 5", () => {
+    // sending promotion email
+    sendPromotionEmail();
   });
 };
 
 
 
 services();
+promotion();
 
 
 app.listen(PORT, () => {
